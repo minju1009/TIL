20e66447abe56c69abcf8dfb68ed77e5df78c379
minju1009 <90900882+minju1009@users.noreply.github.com>
2021-11-21 23:54:58 +0900

    Update object.js

--- 2021.11 Dreamcoding/JavaScript/object.js
+++ 2021.11 Dreamcoding/JavaScript/object.js
@@ -33,6 +33,81 @@ delete minju.hasJob;
 console.log(minju.hasJob);
 
 // 2. Computed properties
-console.log(minju.name);
-console.log(minju['name']); // 반드시 스트링타입으로 받아와야 함.
+console.log(minju.name); // 코딩할때는 닷을 써서 하는 경우가 맞다
+console.log(minju['name']); // 반드시 스트링타입으로 받아와야 함.  // 실시간으로 원하는 값을 받아오고 싶다면 얘를 씀
+minju['hasJob'] = true;
+console.log(minju.hasJob);
+
+function printValue(obj, key) {
+    console.log(obj[key]);
+}
+
+printValue(minju, 'name');
+printValue(minju, 'age');
+
+// 3. Property value shorthand
+const person1 = {name: 'bob', age: 2};
+const person2 = {name: 'steve', age: 3};
+const person3 = {name: 'dave', age: 4};
+const person4 = makePerson('ellie', 30)
+console.log(person4);
+
+function makePerson(name, age) {
+    return {
+        name,
+        age,
+    }
+}
+
+// 가만보니 이거 클래스네!
+
+// 4. Constructor function
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// 5. in operator: property existence chek (key in obj)
+console.log('name' in minju);
+console.log('age' in minju);
+console.log('random' in minju);
+
+// 6. for..in vs for..of
+// for (key in obj)
+console.clear();
+for (key in minju) {
+    console.log(key);
+}
+
+// for (value of iterable)
+const array = [1,2,4,5];
+for(value of array) {
+    console.log(value);
+}
+
+// 7. Fun cloning
+// Object.assign(dest, [obj1, obj2, obj3...])
+const user = {name:'minju', age:'20'};
+const user2 = user;
+user2.name = 'coder';
+console.log(user); // --> user2가 가르치고 있는 ref의 minju의 이름을 코더로 바꿨음!!
+
+// old way
+const user3 = {};
+for(key in user) {
+    user3[key] = user[key];
+}
+console.clear();
+console.log(user3);
+
+const user4 = {};
+Object.assign(user4, user); //Obeject는 기본적으로 JavaScript에 포함되어 있음
+console.log(user4);
+
+// another example
+const fruit1 = {color: 'red'};
+const fruit2 = {color: 'blue', size: 'big'};
+const mixed = Object.assign({}, fruit1, fruit2});
+console.log(mixed.color); // blue 가 출력됨. 값이 덮어씌워지니깐!
+console.log(mixed.size); 
 
