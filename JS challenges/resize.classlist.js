fd9fa0c81ab369c08a82b6230e89fde23c3bf99b
minju1009 <90900882+minju1009@users.noreply.github.com>
2022-01-14 23:46:58 +0900

    JS Challenge : resize and change using classList

---
+++ JS challenges/resize.classlist.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('body');
+const fullScreenWidth = window.screen.width;
+console.dir(body);
+
+function handleResize() {
+    const currentWidth = window.innerWidth;
+    if(currentWidth <= fullScreenWidth*0.5){
+        body.classList.remove("yellow","violet");
+        body.classList.add("blue");
+    } else if(currentWidth > fullScreenWidth*0.8){
+        body.classList.remove("blue","violet");
+        body.classList.add("yellow");
+    } else if(currentWidth>fullScreenWidth*0.5 && currentWidth <= fullScreenWidth*0.8){
+        body.classList.remove("blue","yellow");
+        body.classList.add("violet");
+    }
+}
+
+window.addEventListener("resize", handleResize);
