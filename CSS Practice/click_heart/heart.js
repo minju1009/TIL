f6ef3c9c6cdd9cd469f87e8769e1a8b8a0d0db85
Minju <minju1009@gmail.com>
2022-01-29 17:51:09 +0900

    update gitignore

--- CSS Practice/click_heart/heart.js
+++
@@ -1,8 +0,0 @@
-const heart = document.querySelector("div");
-const emptyHeart = document.querySelector(".empty")
-const fullHeart = document.querySelector(".full");
-
-heart.addEventListener("click", () => {
-    emptyHeart.classList.toggle("hidden");
-    fullHeart.classList.toggle("hidden");
-})
