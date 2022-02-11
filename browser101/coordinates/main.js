5479528a4e08248e7950ca89bd58b78b703f158c
Minju <minju1009@gmail.com>
2022-02-12 02:24:46 +0900

    update finding coordinates

--- browser101/coordinates/main.js
+++ browser101/coordinates/main.js
@@ -4,6 +4,8 @@
 // 2. 그리고 그 좌표에 이미지를 넣어야 한다.
 // 3. 그 이미지 밑에 x 좌표와 y 좌표가 수치로 따라다녀야 한다.
 // 4. 그 이미지까지 선이 나와야 한다.
+// 4번까지 모두 해결.
+// 페이지 업로드와 동시에 선이 나오도록 하는 부분 추가 학습 필요.
 
 const numberbox = document.querySelector('.coordinates');
 const target = document.querySelector('img');
@@ -15,32 +17,24 @@ function getCoordinates(event){
     const pageX = event.pageX;
     const pageY = event.pageY;
    
-    target.style.position = 'absolute';
     target.style.left = `${pageX - 60}px `;
     target.style.top = `${pageY - 60}px`;
     
     numberbox.innerHTML = `${pageX}px, ${pageY}px`;
-    numberbox.style.position = 'absolute';
     numberbox.style.left = `${pageX + 20}px`;
     numberbox.style.top = `${pageY + 20}px`;
 
-    /*
     row.style.top = `${pageY}px`;
-    row.style.width = window.innerWidth;
-    row.style.height = 1;
+    row.style.width = `${window.innerWidth}px`;
     column.style.left = `${pageX}px`;
-    column.style.height = window.innerHeight;
-    column.style.width = 1; 
-    */
-
-
+    column.style.height = `${window.innerHeight}px`//;
 
 }
 
 function placeTarget(){
-    window.addEventListener('mouseover', getCoordinates)
-};
+    window.addEventListener('mouseover', getCoordinates);
+}
 
-
+document.addEventListener("DOMContentLoaded", getCoordinates);
 setInterval(placeTarget,100);
 
