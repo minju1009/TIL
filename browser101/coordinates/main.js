10f57c9406f1d2e0e850ca287a75e56c4ca1ea4c
Minju <minju1009@gmail.com>
2022-02-13 01:56:39 +0900

    update: finding coordinates, top to translate

--- browser101/coordinates/main.js
+++ browser101/coordinates/main.js
@@ -7,13 +7,37 @@
 // 4번까지 모두 해결.
 // 페이지 업로드와 동시에 선이 나오도록 하는 부분 추가 학습 필요.
 
+
 const numberbox = document.querySelector('.coordinates');
 const target = document.querySelector('img');
 const row = document.querySelector('.row');
 const column = document.querySelector('.column');
 
 
-//엘리님 스타일로 맞춰 다시 써보는 코드
+addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const halfWidth = targetRect.width / 2;
+    const halfHeight = targetRect.height / 2;
+
+// top, left 대신 translate 사용하여 성능개선
+document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    column.style.transform = `translateX(${x}px)`;
+    row.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x-halfWidth}px, ${y-halfHeight}px)`;
+    numberbox.style.transform = `translate(${x}px, ${y}px)`;
+    numberbox.innerHTML = `${x}px, ${y}px`;    
+})
+})
+
+
+
+
+
+//엘리님 스타일로 맞춰 다시 써보는 코드 - version1 (top과 left이용하여 성능이 다소 떨어짐)
+/*
 document.addEventListener('mousemove', (event) => {
     const x = event.clientX;
     const y = event.clientY;
@@ -26,7 +50,7 @@ document.addEventListener('mousemove', (event) => {
     numberbox.style.left = `${x}px`;
     numberbox.innerHTML = `${x}px, ${y}px`;    
 })
-
+*/
 
 
 
