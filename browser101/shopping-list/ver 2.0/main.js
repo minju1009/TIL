eb9a3d36340c3dd53be54cd0b6f2e2d16753953f
Minju <minju1009@gmail.com>
2022-02-15 17:43:10 +0900

    update shopping list using event delegation

--- browser101/shopping-list/ver 2.0/main.js
+++ browser101/shopping-list/ver 2.0/main.js
@@ -2,7 +2,7 @@ const items = document.querySelector('.items');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__btn');
 
-
+// 플러스 버튼 눌렀을 때, 리스트 추가
 function onAdd(){
 
 const text = input.value;
@@ -17,43 +17,67 @@ input.value = '';
 input.focus();
 }
 
+// 버튼 눌렀을 때, 이벤트 추가
 addBtn.addEventListener('click', () => {
     onAdd();
 })
 
+let id = 0;
+// 새로운 리스트 만들기
 function createItem(text){
 const itemRow = document.createElement('li');
 itemRow.setAttribute('class', 'item__row');
+itemRow.setAttribute('data-id', id);
+itemRow.innerHTML = 
+    `<div class="item">
+        <span class="item__name">${text}</span>
+        <button class="deleteBtn">
+            <i class="fa fa-solid fa-trash" data-id="${id}"></i>
+     </button>
+    </div>
+    <div class="item__divider"></div>`;
+id++;
 
-const item = document.createElement('div');
-item.setAttribute('class', 'item');
+// itemRow.setAttribute('class', 'item__row');
 
-const itemName= document.createElement('span');
-itemName.setAttribute('class', 'item__name');
-itemName.innerHTML = text;
+// const item = document.createElement('div');
+// item.setAttribute('class', 'item');
 
-const deleteBtn = document.createElement('button');
-deleteBtn.setAttribute('class', 'deleteBtn');
-deleteBtn.innerHTML = '<i class="fa fa-solid fa-trash"></i>'
-deleteBtn.addEventListener('click', () => {
-    items.removeChild(itemRow);
-})
+// const itemName= document.createElement('span');
+// itemName.setAttribute('class', 'item__name');
+// itemName.innerHTML = text;
 
-const itemDivider = document.createElement('div');
-itemDivider.setAttribute('class', 'item__divider');
+// const deleteBtn = document.createElement('button');
+// deleteBtn.setAttribute('class', 'deleteBtn');
+// deleteBtn.innerHTML = '<i class="fa fa-solid fa-trash"></i>'
 
-itemRow.appendChild(item);
-itemRow.appendChild(itemDivider);
+// items.addEventListener('click', (event) => if(event.target.tagName === 'LI'){
+//     items.removeChild(itemRow);
+// })
 
-item.appendChild(itemName);
-item.appendChild(deleteBtn);
+// const itemDivider = document.createElement('div');
+// itemDivider.setAttribute('class', 'item__divider');
+
+// itemRow.appendChild(item);
+// itemRow.appendChild(itemDivider);
+
+// item.appendChild(itemName);
+// item.appendChild(deleteBtn);
 
 return itemRow;
-
 }
 
 input.addEventListener('keypress', (event)=>{
     if(event.key ==='Enter'){
         onAdd();
+    }
+})
+
+
+items.addEventListener('click', (event) => {
+    const id = event.target.dataset.id;
+    if(id){
+        const tobeDeleted = document.querySelector(`.item__row[data-id="${id}"]`); 
+        tobeDeleted.remove();
     }
 })
