afc306bfe39d90548d28cf5326fad401a77ec4ce
Minju <minju1009@gmail.com>
2022-03-07 19:25:44 +0900

    update login page with JS

--- westagram/JS/login.js
+++ westagram/JS/login.js
@@ -1,4 +1,19 @@
-const id = document.querySelector('.loginForm__id');
-const pwd = document.querySelector('.loginForm__pwd');
-const loginBtn = document.querySelector('.loginForm__submit');
+const loginForm = document.querySelector('.loginForm');
+
+loginForm.addEventListener('keydown', checkValidation);
+
+function checkValidation(){
+    const id = document.querySelector('.loginForm__id').value.length;
+    const pwd = document.querySelector('.loginForm__pwd').value.length;
+    const loginBtn = document.querySelector('.loginForm__submit');
+    if(id >= 1 && pwd >=1){
+        loginBtn.classList.add('checked');
+    }else{
+        loginBtn.classList.remove('checked');
+    }
+    
+}
+
+
+
 
